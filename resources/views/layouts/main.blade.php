1ad43ca6822c42ee972cd91198ee674d42b609f2
uranium-boy <janczara.maciej@gmail.com>
2025-01-08 20:13:59 +0100

    Working test page (added layouts that works with guests) + main layout + guest-navbar layout ? what does public layout do?

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', config('app.name', 'Laravel'))</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,22 +16,20 @@
 </head>
 <body class="font-sans antialiased">
 <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-    @include('layouts.navigation')
-
-
+    @include('layouts.guest-navbar')
 
     <!-- Page Heading -->
     @isset($header)
         <header class="bg-white dark:bg-gray-800 shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
+                @yield('header')
             </div>
         </header>
     @endisset
 
     <!-- Page Content -->
     <main>
-        {{ $slot }}
+        @yield('content')
     </main>
 </div>
 </body>
