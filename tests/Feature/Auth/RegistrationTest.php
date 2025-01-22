4b7ab5cb71da2e3f9bc75d3a0e3af22a9b6e548d
Maciej Janczara <115871430+uranium-boy@users.noreply.github.com>
2025-01-22 19:56:06 +0100

    Update RegistrationTest.php

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -10,6 +10,11 @@ test('new users can register', function () {
     $response = $this->post('/register', [
         'name' => 'Test User',
         'email' => 'test@example.com',
+        'telephone' => '123456789',
+        'street_address' => '123 Main St',
+        'city' => 'Test City',
+        'postal_code' => '12345',
+        'birthdate' => '1990-01-01',
         'password' => 'password',
         'password_confirmation' => 'password',
     ]);
